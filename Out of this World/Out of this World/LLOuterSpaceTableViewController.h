1ff2a3c312d3ba06f1d430c11e5d15c92fcdb011
Loc Le <ducloc1803@gmail.com>
2014-06-17 16:44:35 -0700

    finish 101

---
+++ Out of this World/Out of this World/LLOuterSpaceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by LOC LE on 6/17/14.
+//  Copyright (c) 2014 LOC LE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLOuterSpaceTableViewController : UITableViewController
+
+@end
---
+++ Out of this World/Out of this World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by LOC LE on 6/17/14.
+//  Copyright (c) 2014 LOC LE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LLAppDelegate class]));
+    }
+}
